2fb2a709c475debc3f146018471bb202ae71da95
Greilyn Polanco <greilyntejada03@gmail.com>
2023-03-20 19:25:43 -0400

    Creando mi primera migracion

---
+++ Migrations/20230320231833_Inicial.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Parcial2_AP1_GreilynPolanco.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Concepto = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductosDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Cantidad = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductosDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_ProductosDetalle_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "Concepto", "Fecha" },
+                values: new object[,]
+                {
+                    { 1, "Many", new DateTime(2023, 3, 20, 0, 0, 0, 0, DateTimeKind.Local) },
+                    { 2, "Pasas", new DateTime(2023, 3, 20, 0, 0, 0, 0, DateTimeKind.Local) },
+                    { 3, "Ciruela", new DateTime(2023, 3, 20, 0, 0, 0, 0, DateTimeKind.Local) },
+                    { 4, "Arandanos", new DateTime(2023, 3, 20, 0, 0, 0, 0, DateTimeKind.Local) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductosDetalle_ProductoId",
+                table: "ProductosDetalle",
+                column: "ProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductosDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
