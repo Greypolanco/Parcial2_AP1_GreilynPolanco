db1b89b5bd0e12948329a10a212925e705d835c6
Greilyn Polanco <greilyntejada03@gmail.com>
2023-03-20 20:54:36 -0400

    agregando peso

--- Models/ProductosDetalle.cs
+++ Models/ProductosDetalle.cs
@@ -2,6 +2,8 @@ public class ProductosDetalle
 {
     [Key]
     public int DetalleId { get; set; }
+    public int ProductoId { get; set; }
     public string Descripcion { get; set; } = string.Empty;
     public int Cantidad { get; set; }
+    public float Peso { get; set; }
 }
