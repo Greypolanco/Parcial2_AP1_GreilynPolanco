176dae57eb5c95975319cc4caf823341431b2a51
Greilyn Polanco <greilyntejada03@gmail.com>
2023-03-23 23:43:19 -0400

    Modificado a EmpacadoDetalle

---
+++ Models/EmpacadosDetalle.cs
@@ -0,0 +1,8 @@
+public class EmpacadosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int ProductoId { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public int Cantidad { get; set; }
+}
