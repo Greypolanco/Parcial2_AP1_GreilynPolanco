cf6a6bdf7a3e96f0ed273084545bc88fa7541b9c
Greilyn Polanco <greilyntejada03@gmail.com>
2023-03-23 23:44:38 -0400

    Agg entidad Empacados al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -2,6 +2,7 @@ public class Contexto : DbContext
 {
     #nullable disable
     public DbSet<Productos> Productos { get; set; }
+    public DbSet<Empacados> Empacados { get; set; }
 
     public Contexto(DbContextOptions<Contexto> options) : base(options)
     {
@@ -13,26 +14,34 @@ public class Contexto : DbContext
         modelBuilder.Entity<Productos>().HasData(
             new Productos{
                 ProductoId =1,
-                Fecha = DateTime.Today,
-                Concepto = "Many"
+                Costo=30,
+                Precio =50,
+                Existencia=30,
+                Descripcion = "Many"
             },
 
             new Productos{
                 ProductoId =2,
-                Fecha = DateTime.Today,
-                Concepto = "Pasas"
+                Costo=20,
+                Precio =40,
+                Existencia=25,
+                Descripcion = "Pasas"
             },
 
             new Productos{
                 ProductoId =3,
-                Fecha = DateTime.Today,
-                Concepto = "Ciruela"
+                Costo=50,
+                Precio =70,
+                Existencia=30,
+                Descripcion = "Ciruela"
             },
 
             new Productos{
                 ProductoId =4,
-                Fecha = DateTime.Today,
-                Concepto = "Arandanos"
+                Costo=65,
+                Precio =90,
+                Existencia=20,
+                Descripcion = "Arandanos"
             }
 
         );
