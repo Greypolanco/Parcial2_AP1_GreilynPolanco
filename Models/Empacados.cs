981f92490e6daf64ee3a8231b8cc9df0c5b9a706
Greilyn Polanco <greilyntejada03@gmail.com>
2023-03-25 17:51:30 -0400

    Modificado

--- Models/Empacados.cs
+++ Models/Empacados.cs
@@ -5,7 +5,8 @@ public class Empacados
     public int EmpacadoId { get; set; }
     public DateOnly Fecha { get; set; } 
     public string Concepto { get; set; } = string.Empty;
-    public int Cantidad { get; set; }
     [ForeignKey("EmpacadoId")]
     public virtual List<EmpacadosDetalle> EmpacadosDetalle { get; set; } = new List<EmpacadosDetalle>();
+    public int Cantidad { get; set; }
+    public int ProductoId {get; set;}
 }
