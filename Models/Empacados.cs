dc8a8027f0ad3d4c9d07bba04f87c28bd6c0ebce
Greilyn Polanco <greilyntejada03@gmail.com>
2023-03-25 12:51:12 -0400

    Agregando cantidad a Empacado

--- Models/Empacados.cs
+++ Models/Empacados.cs
@@ -3,8 +3,9 @@ public class Empacados
 {
     [Key]
     public int EmpacadoId { get; set; }
-    public DateTime Fecha { get; set; }
+    public DateOnly Fecha { get; set; } 
     public string Concepto { get; set; } = string.Empty;
+    public int Cantidad { get; set; }
     [ForeignKey("ProductoId")]
     public virtual List<EmpacadosDetalle> EmpacadosDetalle { get; set; } = new List<EmpacadosDetalle>();
 }
