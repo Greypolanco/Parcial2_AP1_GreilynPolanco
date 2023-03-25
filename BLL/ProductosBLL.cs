9f0217f78e3cd040f5b0d23f928b5099f9bbff72
Greilyn Polanco <greilyntejada03@gmail.com>
2023-03-25 12:52:23 -0400

    agg el Buscar

--- BLL/ProductosBLL.cs
+++ BLL/ProductosBLL.cs
@@ -7,6 +7,11 @@ public class ProductosBLL
         this._contexto = _contexto;
     }
 
+    public Productos? Buscar(int ProductoId)
+    {
+        return _contexto.Productos.Where(p => p.ProductoId == ProductoId).AsNoTracking().SingleOrDefault();
+    }
+
     public List<Productos> GetList(Expression<Func<Productos, bool>>criterios){
         return _contexto.Productos.AsNoTracking().Where(criterios).ToList();
     }
